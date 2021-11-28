866a549b4863a4a6fb3af8c6fc795083fcaf303d
daniil-kiryanov <daniil.kiryanov@gmail.com>
2021-11-28 13:49:05 +0400

    Pushkin

--- app/src/main/java/com/test/androidproject/MainActivity.kt
+++ app/src/main/java/com/test/androidproject/MainActivity.kt
@@ -1,12 +1,47 @@
-
 package com.test.androidproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "Ты видел деву на скале")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "В одежде белой над волнами")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Когда, бушуя в бурной мгле,")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Играло море с берегами,")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Когда луч молний озарял")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Ее всечасно блеском алым")
     }
 }
+
+
